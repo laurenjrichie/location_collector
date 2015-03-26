7dd9076ea8afc3da4ca74d8186f6b3fab8ed0757
laurenjrichie <laurenjrichie@gmail.com>
2015-03-26 13:39:54 -0700

    coordinates exported as csv

--- js/app.js
+++ js/app.js
@@ -8,11 +8,25 @@ function initialize() {
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
   mapOptions);
+  var array = [];
   google.maps.event.addListener(map,'click',function(event) {
-
-    var coordinates = event.latLng.lat() + ', ' + event.latLng.lng();
-    console.log(coordinates)
-  //  document.getElementById('latlng').value = event.latLng.lat() + ', ' + event.latLng.lng()
- });
+    var coordinates = [event.latLng.lat() + ', ' + event.latLng.lng()];
+    array.push(coordinates);
+    console.log(array);
+  });
+  exportCsv(array);
 }
-  google.maps.event.addDomListener(window, 'load', initialize);
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function exportCsv(data) {
+  $('#done-button').on('click', function() {
+    var csvContent = "data:text/csv;charset=utf-8,";
+    data.forEach(function(infoArray, index){
+       dataString = infoArray.join(",");
+       csvContent += index < data.length ? dataString+ "\n" : dataString;
+    });
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+  });
+}
